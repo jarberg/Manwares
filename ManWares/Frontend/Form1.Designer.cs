3fd3050bc91cff68b09c565a87d97f18aa08ff70
jarberg <mjarberg88@gmail.com>
2020-03-01 15:57:23 +0100

    first commit

---
+++ ManWares/Frontend/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace ManWares
+{
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.button1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button1.Location = new System.Drawing.Point(266, 255);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(206, 97);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(760, 106);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(52, 45);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "button2";
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(929, 519);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn2;
+    }
+}
